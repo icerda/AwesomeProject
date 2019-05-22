0cb83ade2d7a16d9860a400b2c2c767b185afbfb
icerda <icerda@create-cook-it.com.mx>
2019-05-21 19:27:05 -0500

    Props

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import React, {Component} from 'react';
-import {AppRegistry, Image} from 'react-native';
+import {AppRegistry, Text, View, Image} from 'react-native';
 
-export default class Bananas extends Component {
+class Greeting extends Component {
     render() {
-        let pic = {
-            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-        };
         return (
-            <Image source={pic} style={{width: 193, height: 110}}/>
+            <View style={{alignItems: 'center'}} >
+                <Text>Hello {this.props.name}! </Text>
+            </View>
+        )
+    }
+
+}
+
+export default class LotsOfGreetings extends Component {
+    render() {
+
+        return (
+
+            <View style={{alignItems: 'center', top: 150}} >
+                <Greeting name='Rexxar' />
+                <Greeting name='Jaina' />
+                <Greeting name='Valeera' />
+            </View>
         );
     }
 }
