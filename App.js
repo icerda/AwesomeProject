801590e609d1f6bc2daf8942224f383378ba365d
icerda <icerda@create-cook-it.com.mx>
2019-05-21 19:51:14 -0500

    Blink

--- App.js
+++ App.js
@@ -1,27 +1,43 @@
 import React, {Component} from 'react';
 import {AppRegistry, Text, View, Image} from 'react-native';
 
-class Greeting extends Component {
+class Blink extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {isShowingText: true};
+
+        setInterval(() => (
+            this.setState(previousState => ({
+                    isShowingText: !previousState.isShowingText
+                })
+            )), 2000);
+
+    }
+
     render() {
+        if(!this.state.isShowingText){
+            return null;
+        }
+
         return (
-            <View style={{alignItems: 'center'}} >
-                <Text>Hello {this.props.name}! </Text>
-            </View>
+            <Text>{this.props.text}</Text>
         )
     }
 
 }
 
-export default class LotsOfGreetings extends Component {
+export default class BlinkApp extends Component {
     render() {
 
         return (
 
-            <View style={{alignItems: 'center', top: 150}} >
-                <Greeting name='Rexxar' />
-                <Greeting name='Jaina' />
-                <Greeting name='Valeera' />
+            <View>
+                <Blink text='I love to blink' />
+                <Blink text='Yes blinking is so great' />
+                <Blink text='Why did they ever take this out of HTML' />
+                <Blink text='Look at me look at me look at me' />
             </View>
+
         );
     }
 }
